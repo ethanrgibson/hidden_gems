565bc0943a96bf87b577b0df4d1f72008838dae5
Justin Morrow <justintwomorrow@gmail.com>
2025-04-02 14:50:10 -0600

    that shit right here

---
+++ client/src/components/BlogCardComponents.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <div class="image-fluid">
+          <img
+            src="https://images.unsplash.com/photo-1600284536251-8bb98db53468?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="stair way to heven">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped></style>
