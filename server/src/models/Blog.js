1ee105e42c4880c933f8402b7993b706fc903fa5
ethanrgibson <ethanrgibson33@gmail.com>
2025-04-02 15:14:46 -0600

    getting all blogs, working on populating creator

--- server/src/models/Blog.js
+++ server/src/models/Blog.js
@@ -8,7 +8,8 @@ export const BlogSchema = new Schema(
     title: { type: String, required: true, minLength: 3, maxLength: 50 },
     body: { type: String, required: true, minLength: 15, maxLength: 5000 },
     coverImg: { type: String, required: true, minLength: 1, maxLength: 1000 },
-    isPublished: { type: Boolean, required: true, default: false }
+    isPublished: { type: Boolean, required: true, default: false },
+    category: { type: String, enum: ['camping', 'hiking', 'overlanding'] }
 
   },
 
--- server/src/services/BlogService.js
+++ server/src/services/BlogService.js
@@ -6,6 +6,10 @@ class BlogService {
     const blog = await dbContext.Blogs.create(blogData)
     return blog
   }
+  async getAllBlogs() {
+    const blogs = dbContext.Blogs.find().populate('creator', 'name picture')
+    return blogs
+  }
 
 }
 
