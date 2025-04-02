f81ea1e3246ba2f314d174e3ffc69a024b76e4c4
ethanrgibson <ethanrgibson33@gmail.com>
2025-04-02 11:53:51 -0600

    we can login

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-6gqadwkl0v55aq8i.us.auth0.com'
+export const clientId = 'hyEonIcDcUxIsrRhxq0pYeGv4eyjMyGr'
+export const audience = 'https://auth.firstappapi.com'
