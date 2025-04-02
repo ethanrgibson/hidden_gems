ecbed6164ae297ee4448012c935ab358169cf7fe
ethanrgibson <ethanrgibson33@gmail.com>
2025-04-02 16:00:30 -0600

    getting a blog by an ID

--- server/src/services/BlogService.js
+++ server/src/services/BlogService.js
@@ -7,9 +7,17 @@ class BlogService {
     await blog.populate('creator', 'name picture')
     return blog
   }
+
+
   async getAllBlogs() {
     const blogs = await dbContext.Blogs.find().populate('creator', 'name picture')
     return blogs
+  }
+
+
+  async getBlogById(blogId) {
+    const blog = await dbContext.Blogs.findById(blogId).populate('creator', 'name picture')
+    return blog
   }
 
 }
