030b012d5d0ffc3911088c53c6d75ed952a8972c
thghost21 <tylerhacker21@gmail.com>
2025-04-02 15:55:12 -0600

    fooooooter

--- client/src/App.vue
+++ client/src/App.vue
@@ -12,11 +12,38 @@ import { RouterView } from 'vue-router';
     <RouterView/>
   </main>
   <footer class=" text-center">
-    Made with <i class="mdi mdi-heart text-pink"></i> by CodeWorks
+    <section class="container-fluid">
+      <div class="row bg-success">
+        <div class="col-12 py-4 bg-success">
+          <div class="row">
+            <div class="col-3">
+              <a href="">Camping</a>
+            </div>
+            <div class="col-3">
+              <a href="">Hiking</a>
+            </div>
+            <div class="col-3">
+              <a href="">Overland</a>
+            </div>
+            <div class="col-3">
+              <a href="">About</a>
+            </div>
+          </div>
+          <div class="col-12">
+            <img class="logo" src="@/assets/img/logo.svg" height="90" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
   </footer>
 </template>
 
 <style lang="scss">
-
+a{
+  color: black;
+}
+.logo{
+  filter: invert(55%) sepia(76%) saturate(1433%) hue-rotate(351deg) brightness(101%) contrast(101%);
+}
 
 </style>
