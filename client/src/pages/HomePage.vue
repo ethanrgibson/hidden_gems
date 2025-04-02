27175d3b3bc89f34d436e163266fd7bc3ba58be7
thghost21 <tylerhacker21@gmail.com>
2025-04-02 15:58:05 -0600

    hey

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -6,8 +6,6 @@
 
 <!-- TODO import bg image from local storage -->
 <template>
-<<<<<<< HEAD
-<<<<<<< HEAD
   <div class="text-center">
     <h1>Blog Name</h1>
   </div>
@@ -89,67 +87,7 @@
         </div>
       </div>
     </div>
-    <div class="row justify-content-center">
-      <div class="col-12 px-0 campfire-bg">
-        <div class="campfire d-flex justify-content-start pt-4 ">
-          <div class="text-white-shadow px-5">
-            <h1 class="py-2">Come Join the Campfire</h1>
-            <RouterLink :to="{ name: 'Campfire' }">
-              <button class="btn btn-warning">See What Everyone is Saying</button>
-            </RouterLink>
-          </div>
-        </div>
-      </div>
-      <div class="col-12">
-        <div class="border-bottom border-warning my-3 p-2 text-end">
-          <h2>Explore With Others</h2>
-        </div>
-      </div>
-      <div class="col-md-7 py-3">
-        <div class="d-flex align-items-center justify-content-between bg-light shadow p-4">
-          <div>
-            <img class="img-fluid activity-tile"
-              src="https://images.unsplash.com/photo-1621412847056-259b014ef157?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTEyfHxjYW1waW5nfGVufDB8MHwwfHx8Mg%3D%3D"
-              alt="">
-          </div>
-          <div class="text-center">
-            <h3>Camping</h3>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-7 py-3">
-        <div class="d-flex align-items-center justify-content-between bg-light shadow p-4">
-          <div class="text-center">
-            <h3>Hiking</h3>
-          </div>
-          <div>
-            <img class="img-fluid activity-tile"
-              src="https://images.unsplash.com/photo-1551632811-561732d1e306?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aGlraW5nfGVufDB8MHwwfHx8Mg%3D%3D"
-              alt="">
-          </div>
-        </div>
-      </div>
-      <div class="col-md-7 py-3">
-        <div class="d-flex align-items-center justify-content-between bg-light shadow p-4">
-          <div>
-            <img class="img-fluid activity-tile"
-              src="https://images.unsplash.com/photo-1636138105136-a86084b4e1d9?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8b3ZlcmxhbmR8ZW58MHwwfDB8fHwy"
-              alt="">
-          </div>
-          <div class="text-center">
-            <h3>Overland</h3>
-          </div>
-        </div>
-      </div>
-    </div>
-
-  </div>
-=======
-  <p>Hidden Gems forever</p>
->>>>>>> 7c024f4 (Home Page Place Holder)
-=======
-  <p>heyo</p>
->>>>>>> b683fa9 (super basic nav bar)
+    </section>
 </template>
 
 <style scoped lang="scss">
@@ -170,6 +108,12 @@
 .text-white-shadow {
   text-shadow: 1px 1px 5px black;
   color: white;
+}
+
+.activity-tile {
+  min-height: 40dvh;
+  object-position: center;
+  object-fit: cover;
 }
 
 .activity-tile {
