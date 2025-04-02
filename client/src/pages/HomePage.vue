d008fb089bc861100696c85e2c39a62c5fb9092d
thghost21 <tylerhacker21@gmail.com>
2025-04-02 16:39:53 -0600

    campfire page button working

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -6,7 +6,7 @@
 
 <!-- TODO import bg image from local storage -->
 <template>
-  <section class="container-fluid">
+  <section class="container-fluid bg-light ">
     <div class="row ">
       <div class="col-12 px-0">
         <div class="title-bg-img d-flex justify-content-end align-items-center px-4 ">
