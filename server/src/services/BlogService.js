e9541a4ef9d77e8d67a8f9ed56dc47ca14526211
ethanrgibson <ethanrgibson33@gmail.com>
2025-04-02 16:34:07 -0600

    able to edit blogs

--- server/src/services/BlogService.js
+++ server/src/services/BlogService.js
@@ -1,4 +1,5 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest } from "../utils/Errors.js"
 
 class BlogService {
 
@@ -18,6 +19,23 @@ class BlogService {
   async getBlogById(blogId) {
     const blog = await dbContext.Blogs.findById(blogId).populate('creator', 'name picture')
     return blog
+  }
+
+
+  async editBlogById(blogId, blogData, userInfo) {
+
+    const blogUpdated = await dbContext.Blogs.findById(blogId).populate('creator', 'name picture')
+    if (blogUpdated.creatorId != userInfo.id) {
+      throw new BadRequest('YOU CANNOT EDIT A BLOG YOU DID NOT CREATE')
+    }
+    blogUpdated.title = blogData.title
+    blogUpdated.body = blogData.body
+    blogUpdated.coverImg = blogData.coverImg
+    blogUpdated.category = blogData.category
+
+    await blogUpdated.save()
+    return blogUpdated
+
   }
 
 }
