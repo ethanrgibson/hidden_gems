ceb56dead8bfa0a26b4e765492af974245412e82
ethanrgibson <ethanrgibson33@gmail.com>
2025-04-02 13:51:28 -0600

    blogLocation detailed

--- server/src/models/BlogLocation.js
+++ server/src/models/BlogLocation.js
@@ -3,7 +3,8 @@ import { Schema } from "mongoose"
 export const BlogLocationSchema = new Schema(
 
   {
-
+    locationId: { type: Schema.ObjectId, required: true, ref: 'Location' },
+    blogId: { type: Schema.ObjectId, required: true, ref: 'Blog' }
   }
 
 )
