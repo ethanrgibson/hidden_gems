83fd9b1883a1a5ad9358717c44a6fa220c289fc3
thghost21 <tylerhacker21@gmail.com>
2025-04-02 16:39:53 -0600

    campfire page button working

--- client/src/router.js
+++ client/src/router.js
@@ -12,6 +12,11 @@ const routes = [
     component: loadPage('HomePage')
   },
   {
+    path: '/campfire',
+    name: 'Campfire',
+    component: loadPage('CampfirePage')
+  },
+  {
     path: '/about',
     name: 'About',
     component: loadPage('AboutPage')
