493545060b84237fe32d48b34545e42e8b6264c3
Justin Morrow <justintwomorrow@gmail.com>
2025-04-02 14:51:38 -0600

    another one

--- client/src/components/BlogCardComponents.vue
+++ client/src/components/BlogCardComponents.vue
@@ -6,11 +6,33 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-6">
-        <div class="image-fluid">
-          <img
-            src="https://images.unsplash.com/photo-1600284536251-8bb98db53468?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="stair way to heven">
+      <div class="col-8  d-flex">
+        <div class="card d-flex justify-content-end">
+          <div class=" d-flex">
+            <img class="profile-pic"
+              src="https://images.unsplash.com/photo-1600284536251-8bb98db53468?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="stair way to heven">
+            <div>
+              <h1 class="ms-3">profile Name </h1>
+              <div class="mb-3">
+                <span class="mx-3">date </span>
+                <span class="ms-5 mb-3"> Location </span>
+
+              </div>
+              <p class="text-center justify-content-center d-flex ms-4">Lorem ipsum, dolor sit amet consectetur
+                adipisicing
+                elit.
+                Obcaecati asperiores
+                consequuntur nulla
+                perspiciatis libero beatae reprehenderit inventore temporibus maxime id molestiae, eum delectus amet
+                expedita et ducimus magnam. Placeat, quos.</p>
+            </div>
+            <div class="justify-content-end fs-3 p-2 d-flex gap-3 ">
+              <span>0</span>
+              <i class="mdi mdi-campfire"></i>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
@@ -18,4 +40,9 @@
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.profile-pic {
+  height: 15rem;
+  object-fit: cover;
+}
+</style>
