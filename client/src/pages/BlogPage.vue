0a3c244d6a3a4961af03bb3d1c680403b4723feb
Justin Morrow <justintwomorrow@gmail.com>
2025-04-03 09:47:01 -0600

    kmnwpockjqe

---
+++ client/src/pages/BlogPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <h1>Blog Name</h1>
+  <i class="mdi mdi-icon"></i>
+  <hr>
+
+</template>
+
+
+<style lang="scss" scoped></style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -4,44 +4,86 @@
 </script>
 
 <template>
+  <div class="text-center">
+    <h1>Blog Name</h1>
+  </div>
+  <div>
+    account name
+  </div>
+  <hr>
   <div class="container">
-    <div class="row">
-      <div class="col-8  d-flex">
-        <div class="card d-flex justify-content-end">
-          <div class=" d-flex">
-            <img class="profile-pic"
-              src="https://images.unsplash.com/photo-1600284536251-8bb98db53468?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="stair way to heven">
-            <div>
-              <h1 class="ms-3">profile Name </h1>
-              <div class="mb-3">
-                <span class="mx-3">date </span>
-                <span class="ms-5 mb-3"> Location </span>
 
-              </div>
-              <p class="text-center justify-content-center d-flex ms-4">Lorem ipsum, dolor sit amet consectetur
-                adipisicing
-                elit.
-                Obcaecati asperiores
-                consequuntur nulla
-                perspiciatis libero beatae reprehenderit inventore temporibus maxime id molestiae, eum delectus amet
-                expedita et ducimus magnam. Placeat, quos.</p>
-            </div>
-            <div class="justify-content-end fs-3 p-2 d-flex gap-3 ">
-              <span>0</span>
-              <i class="mdi mdi-campfire"></i>
-            </div>
-
-          </div>
-        </div>
+    <div col-6 class="text-center">
+      <div class="text-center">
+        <i class=" fs-2 mdi mdi-arrow-left-bold-box"></i>
+        <img class="main-blog-img"
+          src="https://images.unsplash.com/photo-1525811902-f2342640856e?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="camp">
+        <span class=" fs-2 mdi mdi-arrow-right-bold-box"></span>
       </div>
     </div>
+    <div class="container justify-content-end">
+      <div col-3>
+        <button class=" justify-content-end btn btn-orange ms-5">camp button</button>
+
+      </div>
+
+    </div>
+
   </div>
+
+
+  <div class="container ">
+    <div col-7 class="text-center ">
+      <p class="text-center my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut assumenda sequi unde
+        voluptate aut non suscipit minus ipsa id! Animi quibusdam magni quis facilis voluptatum. Quisquam, porro atque
+        similique eum vitae optio nam labore maiores, temporibus architecto molestiae quibusdam asperiores. Vero
+        reiciendis hic placeat ea quidem possimus, cupiditate eaque debitis consectetur facere perspiciatis quis id
+        consequuntur labore ipsam maxime magni repellendus aspernatur dolore quia enim porro ab illo. Itaque harum
+        placeat rem sapiente autem, omnis deserunt ipsam adipisci rerum laudantium quod quisquam amet facilis
+        perferendis veniam praesentium corrupti! Officiis ullam neque, facilis voluptatibus beatae nam, nesciunt
+        consequuntur ducimus ipsa earum reiciendis ea saepe amet tenetur! Optio doloremque nam ex, consequatur ipsum
+        laboriosam, modi magnam, quasi soluta aut iusto reiciendis impedit harum dolorum non magni! Asperiores ad vero
+        odit commodi veritatis suscipit debitis ipsam laborum cum, perspiciatis unde esse placeat distinctio, nulla
+        repellat porro nobis exercitationem neque quasi cumque inventore? Alias aliquam, blanditiis architecto totam
+        consectetur reprehenderit! Modi debitis sequi earum, quisquam expedita enim numquam. Sit, consequatur cum.
+        Quaerat error odio modi harum cumque eveniet possimus esse? Itaque eveniet omnis officia maxime accusamus, a
+        delectus animi hic nostrum debitis quae qui assumenda est nisi rerum quidem asperiores. Aliquid itaque libero
+        fuga porro temporibus, delectus voluptate nihil sapiente laudantium a enim quam eligendi doloremque nemo non
+        minima aperiam voluptatibus, eos, est quibusdam placeat maiores. Vero provident quod tempore facere repellat
+        nobis sunt voluptates odio minus? Facilis unde quibusdam exercitationem soluta quasi, sit nobis illum tempore
+        quas optio blanditiis eaque, dolores necessitatibus, aliquid porro odio illo adipisci! Obcaecati nesciunt,
+        aliquam quasi possimus iusto ullam non unde ratione? Minima odio blanditiis corrupti suscipit optio culpa neque
+        perspiciatis praesentium, amet, qui excepturi dolor quo, sequi asperiores illum. Harum veniam quas molestias,
+        dolore sit earum quo nulla iure nemo fugit illum. Aliquam repudiandae ratione similique nam, laudantium labore
+        facere corporis non modi accusamus aperiam voluptatem. Iusto perspiciatis ex odit a suscipit magnam tempore
+        deserunt mollitia libero repellat nulla sequi, voluptatum velit enim expedita natus veritatis minus ea
+        asperiores sit! Aperiam neque eaque sapiente aliquam est, itaque obcaecati voluptas culpa ab tenetur suscipit,
+        ipsum repellendus soluta. Culpa dolorum explicabo, voluptatum consequatur sequi sint eveniet iste laborum
+        debitis nostrum aperiam recusandae doloribus odio vel, in quaerat, ea sit obcaecati dicta delectus corrupti
+        temporibus magnam! Quisquam perspiciatis quidem doloribus in dolorem est! Quas, reiciendis et eos minus minima
+        eligendi amet nostrum tempore deleniti, impedit quaerat voluptatum dolores nobis esse sapiente cupiditate rem
+        doloremque! Illum, itaque, voluptatibus iste consectetur excepturi doloribus dolore iusto distinctio nostrum
+        totam quia fugit sequi dolor accusamus expedita adipisci rerum tempora. Magnam cumque itaque provident ex,
+        repellendus excepturi aliquam hic eveniet expedita quibusdam facere architecto, id dolores eius, illum
+        doloribus! Ipsa, porro nostrum odio facere amet magnam reiciendis tempora, distinctio vero sapiente obcaecati
+        qui sit tenetur dolores aut, numquam maxime assumenda soluta beatae. Fugit, optio ducimus iste veritatis
+        excepturi laudantium nobis totam quidem cupiditate commodi officiis maiores pariatur placeat, dolor
+        exercitationem, eligendi molestiae? Quae at dignissimos inventore? Ab incidunt, illum et pariatur dolor numquam
+        earum beatae excepturi deleniti harum reiciendis culpa ratione nulla tempora? Accusantium corrupti dolorem,
+        nesciunt modi impedit numquam odio laboriosam ad minus autem eos non corporis ullam quis itaque necessitatibus
+        delectus dicta quidem tenetur iusto ducimus eveniet ut. Voluptatum qui vel iure.</p>
+    </div>
+  </div>
+
+
+
 </template>
 
 <style scoped lang="scss">
-.profile-pic {
-  height: 15rem;
-  object-fit: cover;
+.main-blog-img {
+  height: 35rem;
+
+
 }
 </style>
