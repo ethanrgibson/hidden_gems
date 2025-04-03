3d37d5dca3b4597cd893fdd1a138abd6f162f53d
Justin Morrow <justintwomorrow@gmail.com>
2025-04-03 16:47:00 -0600

    fixed navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -16,6 +16,7 @@ watch(theme, () => {
 
 </script>
 
+
 <template>
   <nav class=" text-light navbar navbar-expand-md bg-dark border-bottom border-warning">
     <div class="container-fluid gap-2">
@@ -25,65 +26,56 @@ watch(theme, () => {
           <b class="fs-5"></b>
         </RouterLink>
       </div>
-      <template>
-        <nav class=" text-light navbar navbar-expand-md bg-dark border-bottom border-warning">
-          <div class="container-fluid gap-2">
-            <div>
-              <RouterLink :to="{ name: 'Home' }" class="d-flex align-items-center text-light">
-                <img class="navbar-brand logo" alt="logo" src="@/assets/img/logo.svg" height="80" />
-                <b class="fs-5"></b>
-              </RouterLink>
-            </div>
-            <!-- collapse button -->
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <RouterLink :to="{ name: 'Campfire' }">
+      <!-- collapse button -->
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <RouterLink :to="{ name: 'Campfire' }">
 
-                  <span type="button" class="nav-link bg-orange text-light ">The Campfire</span>
-                </RouterLink>
-                =======
-                <span type="button" class="nav-link bg-orange text-light ">The Campfire</span>
-                >>>>>>> 8b628b2 (super basic nav bar)
-              </li>
-              <li class="nav-item bg-blue">
-                <span type="button" class="nav-link text-light">Camping</span>
-              </li>
-              <li class="nav-item bg-success">
-                <span type="button" class="nav-link text-light">Hiking</span>
-              </li>
-              <li class="nav-item bg-primary">
-                <span type="button" class="nav-link text-light">Overland</span>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-          <!-- LOGIN COMPONENT HERE -->
-          <div class="ms-auto">
+            <span type="button" class="nav-link bg-orange text-light ">The Campfire</span>
+          </RouterLink>
+          =======
+          <span type="button" class="nav-link bg-orange text-light ">The Campfire</span>
+          >>>>>>> 8b628b2 (super basic nav bar)
+        </li>
+        <li class="nav-item bg-blue">
+          <span type="button" class="nav-link text-light">Camping</span>
+        </li>
+        <li class="nav-item bg-success">
+          <span type="button" class="nav-link text-light">Hiking</span>
+        </li>
+        <li class="nav-item bg-primary">
+          <span type="button" class="nav-link text-light">Overland</span>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+    <!-- LOGIN COMPONENT HERE -->
+    <div class="ms-auto">
 
-          </div>
-          <Login />
-        </nav>
-      </template>
+    </div>
+    <Login />
+  </nav>
+</template>
 
-      <style lang="scss" scoped>
-        a {
-          text-decoration: none;
-        }
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+}
 
-        .nav-link {
-          text-transform: uppercase;
-        }
+.nav-link {
+  text-transform: uppercase;
+}
 
-        .navbar-nav .router-link-exact-active {
-          border-bottom: 2px solid var(--bs-success);
-          border-bottom-left-radius: 0;
-          border-bottom-right-radius: 0;
-        }
+.navbar-nav .router-link-exact-active {
+  border-bottom: 2px solid var(--bs-success);
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+}
 
-        .logo {
-          filter: invert(55%) sepia(76%) saturate(1433%) hue-rotate(351deg) brightness(101%) contrast(101%);
-        }
-      </style>
+.logo {
+  filter: invert(55%) sepia(76%) saturate(1433%) hue-rotate(351deg) brightness(101%) contrast(101%);
+}
+</style>
--- client/src/pages/ActiveBlogPage.vue
+++ client/src/pages/ActiveBlogPage.vue
@@ -1,38 +1,42 @@
 <script setup>
 
-
 </script>
 
 
-<!-- TODO import bg image from local storage -->
 <template>
+
   <div class="text-center">
-    <h1>Blog Name</h1>
+    <h1 class="fw-bold">Blog Name</h1>
   </div>
   <div>
     account name
   </div>
+  <i class="mdi mdi-campfire"></i>
   <hr>
   <div class="container">
-
     <div class="text-center">
       <div class="text-center">
         <i class=" fs-2 mdi mdi-arrow-left-bold-box"></i>
-        <img class="main-blog-img"
+        <img class="main-image-here"
           src="https://images.unsplash.com/photo-1525811902-f2342640856e?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="camp">
         <span class=" fs-2 mdi mdi-arrow-right-bold-box"></span>
       </div>
     </div>
+    <div>this might just be me but its all working </div>
     <div class="container justify-content-end">
       <div col-3>
         <button class=" justify-content-end btn btn-orange ms-5">camp button</button>
-
+        <button class=" justify-content-end btn btn-orange ms-5">camp type</button>
+        <button class=" justify-content-end btn btn-orange ms-5">camp fire</button>
       </div>
-
     </div>
 
   </div>
+
+
+
+
 
 
   <div class="container ">
@@ -76,103 +80,19 @@
         nesciunt modi impedit numquam odio laboriosam ad minus autem eos non corporis ullam quis itaque necessitatibus
         delectus dicta quidem tenetur iusto ducimus eveniet ut. Voluptatum qui vel iure.</p>
     </div>
-    <div class="col-12">
-      <div class="my-3 p-2">
-        <div class="border-bottom border-warning">
-          <h2>Find Your Next Gem</h2>
-        </div>
-        <div class="mt-2 fs-4 pb-3">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa laborum commodi unde porro quae odio aperiam
-            optio quo. Inventore fugit dolore rem quia error corrupti earum blanditiis sequi a dolor.</p>
-        </div>
-      </div>
-    </div>
-    <div class="row justify-content-center">
-      <div class="col-12 px-0 campfire-bg">
-        <div class="campfire d-flex justify-content-start pt-4 ">
-          <div class="text-white-shadow px-5">
-            <h1 class="py-2">Come Join the Campfire</h1>
-            <RouterLink :to="{ name: 'Campfire' }">
-              <button class="btn btn-warning">See What Everyone is Saying</button>
-            </RouterLink>
-          </div>
-        </div>
-      </div>
-      <div class="col-12">
-        <div class="border-bottom border-warning my-3 p-2 text-end">
-          <h2>Explore With Others</h2>
-        </div>
-      </div>
-      <div class="col-md-7 py-3">
-        <div class="d-flex align-items-center justify-content-between bg-light shadow p-4">
-          <div>
-            <img class="img-fluid activity-tile"
-              src="https://images.unsplash.com/photo-1621412847056-259b014ef157?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTEyfHxjYW1waW5nfGVufDB8MHwwfHx8Mg%3D%3D"
-              alt="">
-          </div>
-          <div class="text-center">
-            <h3>Camping</h3>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-7 py-3">
-        <div class="d-flex align-items-center justify-content-between bg-light shadow p-4">
-          <div class="text-center">
-            <h3>Hiking</h3>
-          </div>
-          <div>
-            <img class="img-fluid activity-tile"
-              src="https://images.unsplash.com/photo-1551632811-561732d1e306?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aGlraW5nfGVufDB8MHwwfHx8Mg%3D%3D"
-              alt="">
-          </div>
-        </div>
-      </div>
-      <div class="col-md-7 py-3">
-        <div class="d-flex align-items-center justify-content-between bg-light shadow p-4">
-          <div>
-            <img class="img-fluid activity-tile"
-              src="https://images.unsplash.com/photo-1636138105136-a86084b4e1d9?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8b3ZlcmxhbmR8ZW58MHwwfDB8fHwy"
-              alt="">
-          </div>
-          <div class="text-center">
-            <h3>Overland</h3>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
+
+  <span>this is the end</span>
+  <img class="main-image-here"
+    src="https://images.unsplash.com/photo-1568576550491-185584b2145a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    alt="campground">
+  <i class="mdi mdi-facebook"></i>
+  <i class="mdi mdi-linkedin"></i>
 </template>
 
-<style scoped lang="scss">
-.title-bg-img {
-  background-image: url('https://images.unsplash.com/photo-1567018265154-78811ad63787?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
-  background-position: center;
-  background-size: cover;
-  min-height: 75dvh;
-}
 
-.campfire-bg {
-  background-image: url('https://images.unsplash.com/photo-1530541930197-ff16ac917b0e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
-  background-position: center;
-  background-size: cover;
-  min-height: 75dvh;
-}
-
-.text-white-shadow {
-  text-shadow: 1px 1px 5px black;
-  color: white;
-}
-
-.activity-tile {
-  min-height: 40dvh;
-  object-position: center;
-  object-fit: cover;
-}
-
-.activity-tile {
-  min-height: 40dvh;
-  object-position: center;
-  object-fit: cover;
+<style lang="scss" scoped>
+.main-image-here {
+  height: 30rem;
 }
 </style>
-<style scoped lang="scss"></style>
