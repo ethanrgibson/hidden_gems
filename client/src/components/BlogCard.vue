85d456d7c441b67bc9b069742427a531a6513eb3
thghost21 <tylerhacker21@gmail.com>
2025-04-03 11:29:02 -0600

    get blogs and minor styling

---
+++ client/src/components/BlogCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Blog } from '@/models/Blog.js';
+
+defineProps({
+  blogProp: {type: Blog, required: true}
+})
+</script>
+
+
+<template>
+  <div>
+    <div>
+      <img src="https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FtcGluZ3xlbnwwfHwwfHx8MA%3D%3D" :alt="'a picture for ' + blogProp + `'s' post`">
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ client/src/models/Blog.js
@@ -0,0 +1,13 @@
+export class Blog {
+  constructor(data) {
+    this.id = data.id
+    this.creatorId = data.creatorId
+    this.title = data.title
+    this.body = data.body
+    this.coverImg = data.coverImg
+    this.isPublished = data.isPublished
+    this.creator = data.creator
+    this.creator = data.creator
+
+  }
+}
