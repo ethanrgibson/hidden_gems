af3a90cd316a72ea2e5e10206e164a25ccc0c907
ethanrgibson <ethanrgibson33@gmail.com>
2025-04-10 10:48:07 -0600

    can now navigate to certain activity pages

---
+++ client/src/pages/ActivityPage.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { useRoute } from 'vue-router';
+
+
+
+const route = useRoute()
+
+</script>
+
+
+<template>
+  <section>
+    <div v-if="route.params.activityName == 'camping'">
+      Waht's up campers?
+    </div>
+    <div v-if="route.params.activityName == 'hiking'">
+      Whats up hikers?
+    </div>
+    <div v-if="route.params.activityName == 'overlanding'">
+      Whats up overlanders?
+    </div>
+  </section>
+</template>
+
+
+
+<style lang="scss" scoped></style>
